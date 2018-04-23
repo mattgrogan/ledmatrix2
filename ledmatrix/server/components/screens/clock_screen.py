0628cc9e4d26642b3c091c9b275cf645367114ce
Matthew Grogan <matt.grogan@gia.edu>
2018-04-23 18:58:51 -0400

    Added current conditions

--- ledmatrix/server/components/screens/clock_screen.py
+++ ledmatrix/server/components/screens/clock_screen.py
@@ -3,7 +3,12 @@ from PIL import Image
 
 from server.components.screens.screen import Screen
 from server.components.layers.text_layer import TextLayer
+from server.components.layers.scrolling_layer import ScrollingLayer
 from server.components.screens.gif_screen_factory import GifScreenFactory
+
+from server.data.current_conditions import NOAA_Current_Observation
+
+STATION = "KLGA"
 
 class ClockScreen(Screen):
 
@@ -15,31 +20,61 @@ class ClockScreen(Screen):
         icon_file = "icons/gif_icons/13533_icon_thumb.gif"
         self.icon = GifScreenFactory().from_icon(icon_file)
 
+        # Create a connection to the NOAA website 
+        self.current_obs = NOAA_Current_Observation(STATION)
+
+        # Create the weather layer
+        self._create_weather_layer()
+
+        # Cache the current weather
+        self._prev_weather = self._weather()
+
     def enter(self):
         self.icon.enter()
+        self.conditions_layer.enter()
 
     def exit(self):
         pass
 
     def suspend(self):
         self.icon.suspend()
+        self.conditions_layer.suspend()
 
     def resume(self):
         self.icon.resume()
+        self.conditions_layer.resume()
 
     def step(self):
         self.icon.step()
+        self.conditions_layer.step()
+
+    def _create_weather_layer(self):
+        # Add the weather layer
+        self.conditions_layer = ScrollingLayer(self.device, TextLayer(self._weather()), xspeed=10)
 
     def _time(self):
         # Return the formatted time
         return time.strftime("%I:%M", time.localtime()).lstrip("0")
+
+    def _weather_changed(self):
+        # Did the weather change?
+
+        if self._weather() != self._prev_weather:
+            self._prev_weather = self._weather()
+            return True
+        else:
+            return False
+
+    def _weather(self):
+        # Return the current weather conditions
+        return self.current_obs["weather"]
 
     def render(self):
 
         # Create a blank background
         bg = Image.new("RGBA", self.device.size)
 
-        # Render the text
+        ### Render the time
         txt = TextLayer(self._time())
         im = txt.render()
 
@@ -50,9 +85,26 @@ class ClockScreen(Screen):
         # Paste the text on the background
         bg.paste(im, box=(x, y), mask=im)
 
-        # Render the icon
+        ### Render the icon
         icon_im = self.icon.render()
         bg.paste(icon_im, box=(0,0), mask=icon_im)
+
+        ### Render the temperature
+        temp_str = "%iF" % int(float(self.current_obs["temp_f"]))
+        temp = TextLayer(temp_str)
+        temp_im = temp.render()
+        x = 4
+        y = im.size[1] + 2 # Offset by two rows
+        bg.paste(temp_im, box=(x, y), mask=temp_im)
+
+        ### Render the conditions
+        if self._weather_changed():
+            self._create_weather_layer
+
+        cond_im = self.conditions_layer.render()
+        x = 0
+        y = im.size[1] * 2 + 4
+        bg.paste(cond_im, box=(x, y), mask=cond_im)
 
         return bg
 
---
+++ ledmatrix/server/data/current_conditions.py
@@ -0,0 +1,63 @@
+import requests
+import xml.etree.ElementTree as ET
+
+from server.data.throttle_mixin import Throttle_Mixin
+
+class NOAA_Current_Observation(Throttle_Mixin):
+  """
+  This class handles connections to weather.gov for the current observation.
+  """
+
+  def __init__(self, station):
+
+    self.station = station
+    self.url = "http://w1.weather.gov/xml/current_obs/%s.xml" % station
+
+    self.current_obs = {}   # Dict to hold observation data
+
+    # Use the Mixin to throttle requests
+    self.every(60 * 30, self._fetch_data)
+
+  def __getitem__(self, name):
+    """
+    Checks for new data and returns the appropriate value.
+    """
+
+    # Call mixin
+    self.run_pending()
+
+    if name in self.current_obs.keys():
+      return self.current_obs[name]
+    else:
+      return ""
+
+  def _fetch_data(self):
+
+    # Attempt a connection
+    try:
+      r = requests.get(self.url, timeout=10)
+    except:
+      return False
+
+    if r.status_code == requests.codes.ok:
+      # Parse the data
+      root = ET.fromstring(r.content)
+      for child in root:
+        self.current_obs[child.tag] = child.text
+      return True
+    else:
+      return False
+
+if __name__ == "__main__":
+
+  cc = NOAA_Current_Observation("KLGA")
+
+  print cc["temp_f"]
+
+  print cc.current_obs.keys()
+
+  for key in cc.current_obs.keys():
+    print "%s:   %s" % (key, cc[key])
+  
+  print cc["temp_f"]
+  print cc["observation_time"]
