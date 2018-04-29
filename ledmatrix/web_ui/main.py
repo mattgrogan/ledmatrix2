f04dbfa590ee29a17a7e435961c82a6d0d216934
Matthew Grogan <matt.grogan@gia.edu>
2018-04-29 12:41:50 -0400

    Image shows on web

--- ledmatrix/web_ui/main.py
+++ ledmatrix/web_ui/main.py
@@ -2,12 +2,17 @@ from __future__ import absolute_import
 
 import StringIO
 import time
-from flask import Flask, send_file
+import zmq
+from flask import Flask, send_file, request, render_template
 
 from messaging.image_connection import ImageConnection
+from messaging.command_connection import CommandConnection
 
 IMG_HOST = "localhost"
 IMG_PORT = 5556
+
+CMD_HOST = "localhost"
+CMD_PORT = 5555
 
 MATRIX_WIDTH = 32
 MATRIX_HEIGHT = 32
@@ -15,25 +20,38 @@ IMG_ZOOM = 16
 
 app = Flask(__name__)
 
+image_conn = ImageConnection()
+image_conn.connect(IMG_HOST, IMG_PORT, as_receiver=True)
+
+
+cmd_conn = CommandConnection()
+cmd_conn.connect(CMD_HOST, CMD_PORT, as_receiver=False)
+
 def serve_pil_image(pil_img):
     # https://stackoverflow.com/questions/7877282/how-to-send-image-generated-by-pil-to-browser
     img_io = StringIO.StringIO()
     pil_img.save(img_io, 'PNG')
     img_io.seek(0)
-    return send_file(img_io, mimetype='image/png')
+    return send_file(img_io, mimetype='image/png', cache_timeout=0)
 
-@app.route('/')
+@app.route("/")
+def display_index():
+    return render_template("index.html")
+
+
+@app.route('/api/v1/current_image.png')
 def display_img():
 
-    image_conn = ImageConnection()
-    image_conn.connect(IMG_HOST, IMG_PORT, as_receiver=True)
-
-    im = image_conn.receive()
-
-    while im is None:
+    try:
         im = image_conn.receive()
-        print "."
-        time.sleep(0.25)
+    except zmq.ZMQError:
+        print "COULD NOT FIND IMAGE"
+        return ""
+
+    # while im is None:
+    #      im = image_conn.receive()
+    #      print "."
+    #      time.sleep(0.25)
 
     # Resize
     w = MATRIX_WIDTH * IMG_ZOOM
@@ -44,3 +62,32 @@ def display_img():
         return serve_pil_image(im)
     else:
         return 'No image found'
+
+@app.route('/api/v1/command', methods=["GET", "POST"])
+def command():
+    if request.method == "POST":
+
+        cmd = request.headers["CMD"]
+
+        print "COMMAND IS: %s" % cmd
+
+        if cmd == "KEY_MODE":
+            cmd_conn.send_mode()
+        elif cmd == "KEY_ENTER":
+            cmd_conn.send_enter()
+        elif cmd == "KEY_UP":
+            cmd_conn.send_up()
+        elif cmd == "KEY_DOWN":
+            cmd_conn.send_down()
+        elif cmd == "KEY_LEFT":
+            cmd_conn.send_left
+        elif cmd == "KEY_RIGHT":
+            cmd_conn.send_right()
+        elif cmd == "KEY_PLAYPAUSE":
+            cmd_conn.send_playpause()
+        else:
+            return "Bad Input"
+
+        return cmd
+    else:
+        return "please post"
--- ledmatrix/web_ui/run.bat
+++ ledmatrix/web_ui/run.bat
@@ -1,3 +1,4 @@
 @echo off 
 set FLASK_APP=main.py
+set FLASK_DEBUG=1
 flask run
